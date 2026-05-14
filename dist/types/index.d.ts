ba6723b78c9d641147277cc3b28376ca83cb4d8d
humanbydefinition <hello@textmode.art>
2026-05-14 17:41:30 +0200

    chore: removed distributables repo

--- dist/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { TextmodePlugin } from 'textmode.js';
-/**
- * Creates a textmode.js plugin that provides an accurate glyph matching conversion strategy.
- * @returns A textmode.js plugin instance.
- */
-export declare const createAccurateConversionPlugin: () => TextmodePlugin;
-export type { TextmodeConversionStrategy } from 'textmode.js';
